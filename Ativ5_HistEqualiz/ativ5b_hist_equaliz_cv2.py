b6be1b40f71d6cceb3915f1927d9889682177e73
Lucas Carvalho Flores <lcf@ic.ufal.br>
2024-08-08 15:54:03 -0300

    Adiciona mais imagens a serem equalizadas

--- Ativ5_HistEqualiz/ativ5b_hist_equaliz_cv2.py
+++ Ativ5_HistEqualiz/ativ5b_hist_equaliz_cv2.py
@@ -13,12 +13,38 @@ import matplotlib.pyplot as plt
 
 
 # Imagem default
-path_img = 'ex1.jpg'
+list_path_img = ['ex1.jpg', 'Fig0310(b)(washed_out_pollen_image).tif',
+                 'Fig0323(a)(mars_moon_phobos).tif', 'dazai.png']
 
 # Se foi fornecido outra imagem, pra usar com as dos slides da aula.
 if len(sys.argv) > 1:
-    path_img = sys.argv[1]
+    list_path_img = sys.argv[1:]
 
+for path in list_path_img:
+
+    img_src = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    img_eqlzd = cv2.equalizeHist(img_src)
+
+    cv2.imshow(f'{path} Original', img_src)
+    cv2.imshow(f'{path} Equalizada', img_eqlzd)
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.hist(img_src.ravel(), 256, (0, 256))
+    plt.title(f'{path}\n Histograma Original')
+
+    plt.subplot(1, 2, 2)
+    plt.hist(img_eqlzd.ravel(), 256, (0, 256))
+    plt.title(f'{path}\n Histograma Equalizado')
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+plt.show()
+
+
+'''
 # Carregar a imagem em escala de cinza
 img_orig = cv2.imread(path_img, cv2.IMREAD_GRAYSCALE)
 
@@ -44,3 +70,4 @@ plt.hist(img_eqlzd.ravel(), 256, (0, 256))
 plt.title('Histograma da Imagem Equalizada')
 
 plt.show()
+'''
